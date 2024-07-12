7ef2d01644265cadc5483ad2d34188a259433c39
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-07-12 09:26:43 -0700

    Delete 0-uniq_users.sql

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- This script creates a 'users' table if it does not already exist
-
--- Check if the table exists first to avoid errors
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
