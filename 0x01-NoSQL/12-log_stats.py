a2fc47a455b949aa8d61fe1c815879797dcba000
1manasseh23 <cletavivan@gmail.com>
2024-07-16 06:57:01 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,17 +12,22 @@ collection = db['nginx']
 
 # Get the total number of logs
 total_logs = collection.count_documents({})
+
 print(f"{total_logs} logs")
 
 # Get the count of each HTTP method
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
 print("Methods:")
+
 for method in methods:
     count = collection.count_documents({"method": method})
     print(f"\tmethod {method}: {count}")
+
 
 # Get the count of documents with method=GET and path=/status
 status_checks = collection.count_documents(
         {"method": "GET", "path": "/status"}
         )
+
 print(f"{status_checks} status check")
