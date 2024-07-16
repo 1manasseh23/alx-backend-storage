829e5e874cd71c4cf64a0e92db7e37eff0d8d2e0
1manasseh23 <cletavivan@gmail.com>
2024-07-16 07:19:45 -0700

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+"""This a Python function that returns all
+students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score."""
+    
+    students = list(mongo_collection.find())
+
+    for student in students:
+        scores = [topic['score'] for topic in student.get('topics', [])]
+        student['averageScore'] = sum(scores) / len(scores) if scores else 0
+
+    return sorted(students, key=lambda x: x['averageScore'], reverse=True)
