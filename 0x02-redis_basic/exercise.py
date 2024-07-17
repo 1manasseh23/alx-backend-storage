72ad9316b16ad8ab6601e541de5bddcf2df4cece
1mansseh23 <cletavivan@gmail.com>
2024-07-17 21:13:30 +0000

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,6 +16,26 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Create keys for inputs and outputs
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        # Normalize inputs and append to the inputs list in Redis
+        self._redis.rpush(inputs_key, str(args))
+
+        # Call the original method to get the output
+        output = method(self, *args, **kwargs)
+
+        # Append the output to the outputs list in Redis
+        self._redis.rpush(outputs_key, output)
+
+        return output
+    return wrapper
+
+"""
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -26,13 +46,14 @@ def count_calls(method: Callable) -> Callable:
         # Call the original method
         return method(self, *args, **kwargs)
     return wrapper
+"""
 
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history # @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
