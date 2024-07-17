4cb683741d9e484245ea640a00424aaeaf2fc08c
1mansseh23 <cletavivan@gmail.com>
2024-07-17 21:03:12 +0000

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,15 +12,44 @@ be a str, bytes, int or float
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Get the qualified name of the method
+        key = method.__qualname__
+        # Increment the count in Redis
+        current_count = self._redis.incr(key)
+        # Call the original method
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+            ) -> Optional[Union[str, bytes, int, float]]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
